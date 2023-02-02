925e9f3ba31d15a1fc4aefb1a446f3fe18eb0abe
Rhabi Mendoza <111347175+redpmendoza@users.noreply.github.com>
2023-02-02 09:41:07 +0800

    Add files via upload

---
+++ PlayingWithCharacters.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+
+    // Create ch, s, and sen
+    char ch;
+    char s[100];
+    char sen[100];
+
+    // Get ch, s, and sen
+    scanf("%c", &ch);
+    scanf("%s", &s);
+    scanf("\n");
+    scanf("%[^\n]%*c", &sen);
+    
+    // Output ch, s, and sen
+    printf("%c\n" ,ch);
+    printf("%s\n", s);
+    printf("%s\n", sen);
+}
